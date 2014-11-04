90db1a4055510e57cb222603ce0db7d91a952b2d
Victor Bartel <victor.bartel@dgtresor.gouv.fr>
2014-11-04 22:36:11 +0100

    [Storage] Add to lucene field conversion

--- NBlast.Storage/LogDocument.fs
+++ NBlast.Storage/LogDocument.fs
@@ -1,31 +1,48 @@
 ﻿namespace NBlast.Storage
 
 open Lucene.Net.Documents
+open Lucene.Net
 open System
 
 type StorageType = 
     | Analysed = 1 
     | Stored = 2
-    | Both = 3 
+    | Both = 3
+    
+module FieldUtils = 
+    let toLuceneField name value (storageType: StorageType) =
+        let store = 
+            match storageType with
+                | StorageType.Analysed -> (Documents.Field.Store.NO, Documents.Field.Index.ANALYZED_NO_NORMS)
+                | StorageType.Stored -> (Documents.Field.Store.YES, Documents.Field.Index.NO)
+                | _ -> (Documents.Field.Store.YES, Documents.Field.Index.ANALYZED_NO_NORMS)
+        new Lucene.Net.Documents.Field(name, value, fst store, snd store)
+ 
 
-type Field<'T> = 
-    { Value : 'T
-      Name: string
-      StorageType: StorageType } 
-    member this.ToLuceneField() =
-        let value = match this.Value with
-//            | :? DateTime -> 
-//                DateTools.TimeToString(this.Value :?> DateTime)
-                    | _ -> this.Value.ToString()
-        0
+type Field = 
+    { Value      : string
+      Name       : string
+      StorageType: StorageType }
+
+    member this.ToLuceneField() = 
+        FieldUtils.toLuceneField this.Name this.Value this.StorageType
+        
+type DateTimeField =
+    { Value       : DateTime
+      Name        : String
+      StorageType : StorageType }
+
+    member this.ToLuceneField() = 
+        let value = DateTools.DateToString(this.Value, DateTools.Resolution.SECOND)
+        FieldUtils.toLuceneField this.Name value this.StorageType
 
 
 type LogDocument = 
-    { message  : Field<string>
-      logger   : Field<string>
-      level    : Field<string>
-      error    : Field<string>
-      createdAt: Field<DateTime> }
+    { message  : Field
+      logger   : Field
+      level    : Field
+      error    : Field
+      createdAt: DateTime }
 
     member this.ToLuceneDocument() = 
         
