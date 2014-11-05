1a8ee8b325f0dabb9da3ca0ed7c2b4a5b0d77499
Victor Bartel <victor.bartel@dgtresor.gouv.fr>
2014-11-05 22:52:51 +0100

    [Logger] Finish first version of document creation

--- NBlast.Storage/LogDocument.fs
+++ NBlast.Storage/LogDocument.fs
@@ -6,8 +6,8 @@ open System
 
 type StorageType = 
     | Analysed = 1 
-    | Stored = 2
-    | Both = 3
+    | Stored   = 2
+    | Both     = 3
     
 module FieldUtils = 
     let toLuceneField name value (storageType: StorageType) =
@@ -16,35 +16,55 @@ module FieldUtils =
                 | StorageType.Analysed -> (Documents.Field.Store.NO, Documents.Field.Index.ANALYZED_NO_NORMS)
                 | StorageType.Stored -> (Documents.Field.Store.YES, Documents.Field.Index.NO)
                 | _ -> (Documents.Field.Store.YES, Documents.Field.Index.ANALYZED_NO_NORMS)
-        new Lucene.Net.Documents.Field(name, value, fst store, snd store)
+        new Lucene.Net.Documents.Field(name, value, fst store, snd store) :> IFieldable
  
 
-type Field = 
-    { Value      : string
-      Name       : string
-      StorageType: StorageType }
+type IField<'T> =
+    abstract member Name: unit -> string with get
+    abstract member StorageType: unit -> StorageType with get
+    abstract member Value: unit -> 'T with get
+    abstract member ToLuceneField: unit -> IFieldable
 
-    member this.ToLuceneField() = 
-        FieldUtils.toLuceneField this.Name this.Value this.StorageType
-        
-type DateTimeField =
-    { Value       : DateTime
-      Name        : String
-      StorageType : StorageType }
+type TextField(name        : string, 
+               value       : string, 
+               storageType : StorageType) =
 
-    member this.ToLuceneField() = 
-        let value = DateTools.DateToString(this.Value, DateTools.Resolution.SECOND)
-        FieldUtils.toLuceneField this.Name value this.StorageType
+    new (name, value) = 
+        TextField(name = name, value = value, storageType = StorageType.Both)
+    
+    interface IField<string> with
+        member this.Name with get() = name
+        member this.Value with get() = value
+        member this.StorageType with get() = storageType
+        member this.ToLuceneField() = FieldUtils.toLuceneField name value storageType
 
+type DateTimeField(name        : string,
+                   value       : DateTime,
+                   storageType : StorageType) =
+
+    new (name, value) = 
+        DateTimeField(name = name, value = value, storageType = StorageType.Stored)
+
+    interface IField<DateTime> with
+        member this.Name with get() = name
+        member this.Value with get() = value
+        member this.StorageType with get() = storageType
+        member this.ToLuceneField() = 
+            let value = DateTools.DateToString(value, DateTools.Resolution.SECOND)
+            FieldUtils.toLuceneField name value storageType
 
 type LogDocument = 
-    { message  : Field
-      logger   : Field
-      level    : Field
-      error    : Field
-      createdAt: DateTime }
+    { message   : TextField
+      logger    : TextField
+      level     : TextField
+      error     : TextField
+      createdAt : DateTimeField }
 
     member this.ToLuceneDocument() = 
-        
-        0
-
+        let document = new Document() 
+        (this.message :> IField<string>).ToLuceneField() |> document.Add
+        (this.logger :> IField<string>).ToLuceneField() |> document.Add
+        (this.level :> IField<string>).ToLuceneField() |> document.Add
+        (this.error :> IField<string>).ToLuceneField() |> document.Add
+        (this.createdAt :> IField<DateTime>).ToLuceneField() |> document.Add
+        document
