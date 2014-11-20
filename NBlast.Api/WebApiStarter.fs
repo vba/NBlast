b7801408a1270f2b547bd22c7de149d5aa812c91
Victor Bartel <victor.bartel@dgtresor.gouv.fr>
2014-11-20 16:22:32 +0100

    [Api] Start configuration for IoC part

--- NBlast.Api/WebApiStarter.fs
+++ NBlast.Api/WebApiStarter.fs
@@ -3,22 +3,30 @@
 open Owin
 open System.Web.Http
 open System.Linq
+open Microsoft.Practices.Unity
+open System.Web.Http.Dependencies
 
 type RouteConfig = {
     id : RouteParameter
 }
 
 type WebApiStarter() =
-    member this.Configuration (appBuilder: IAppBuilder): unit =
+    member me.Configuration (appBuilder: IAppBuilder): unit =
         let config = new HttpConfiguration()
 
         config.Routes.MapHttpRoute("DefaultApi",
                                    "api/{controller}/{id}",
                                    {id = RouteParameter.Optional}) |> ignore
 
-        let appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(
-                            fun t -> t.MediaType = "application/xml"
+        let appXmlType = 
+            config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(
+                fun t -> t.MediaType = "application/xml"
         )
         config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType) |> ignore
+        me.ConfigureIoc config
 
         appBuilder.UseWebApi(config) |> ignore
+
+    member private me.ConfigureIoc (config: HttpConfiguration) = 
+        let container = new UnityContainer()
+        config.DependencyResolver = (new UnityResolver(container) :> IDependencyResolver) |> ignore
