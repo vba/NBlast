27e36b92f4de8a17761690fa488862d382083f72
Victor Bartel <victor.bartel@dgtresor.gouv.fr>
2014-11-21 21:14:10 +0100

    [Api] Start to implement indexer controller

---
+++ NBlast.Api/Controllers/IndexerController.fs
@@ -0,0 +1,20 @@
+﻿namespace NBlast.Api
+
+open NBlast.Storage
+open NBlast.Storage.Core.Index
+open System.Web.Http
+open System.Net.Http.Formatting
+
+type IndexerController(storageWriter: IStorageWriter) =
+    inherit ApiController()
+
+
+    [<HttpPost>]
+    //[<Route("index")>]
+    member me.Index sender message logger level =
+        let logDocument = new LogDocument(sender, message, logger, level) :> IStorageDocument
+        logDocument |> storageWriter.InsertOne
+        1
+
+//module IndexerController
+
