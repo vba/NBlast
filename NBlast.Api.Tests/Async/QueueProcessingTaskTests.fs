d74f1349fca2d7e3bc553a3e252d74f9d9ce1786
Victor Bartel <victor.bartel@dgtresor.gouv.fr>
2014-11-27 17:43:18 +0100

    [Test] Cover more specific conversion type for createdAt field

--- NBlast.Api.Tests/Async/QueueProcessingTaskTests.fs
+++ NBlast.Api.Tests/Async/QueueProcessingTaskTests.fs
@@ -7,6 +7,7 @@ open NBlast.Storage
 open NBlast.Storage.Core.Index
 open NBlast.Storage.Core.Extensions
 open System
+open System.Linq.Expressions
 open System.Runtime
 open Xunit
 open FluentAssertions
@@ -113,13 +114,23 @@ type QueueProcessingTaskTests() =
         // Given
         let actualAmount = 20
         let fixture = new Fixture()
+        let expectedCreatedAt = new Nullable<DateTime>(new DateTime(1977, 01, 10))
         let storageWriter = new FakeStorageWriter()
         let queueKeeper = new Mock<IIndexingQueueKeeper>(MockBehavior.Strict)
         let mutable collector: LogDocument list = List.empty
 
         queueKeeper
             .Setup<seq<LogModel>>(fun x -> x.ConsumeMany(Some actualAmount))
-            .Returns(fun () -> fixture.CreateMany<LogModel>(actualAmount)) |> ignore
+            .Returns(fun () -> 
+                [0 .. actualAmount - 1] |> List.map (fun x -> 
+                    new LogModel(Sender=fixture.Create(), 
+                                 Message=fixture.Create(),
+                                 Logger=fixture.Create(),
+                                 Level=fixture.Create(),
+                                 Error=fixture.Create(),
+                                 CreatedAt=expectedCreatedAt)
+                ) |> List.toSeq
+            ) |> ignore
         queueKeeper
             .Setup<int>(fun x -> x.Count()).Returns(fun () -> actualAmount) |> ignore
         
@@ -137,7 +148,7 @@ type QueueProcessingTaskTests() =
             x.Message.Value.Should().NotBeNullOrEmpty("Message field must have a value") |> ignore
             x.Logger.Value.Should().NotBeNullOrEmpty("Logger field must have a value") |> ignore
             x.Level.Value.Should().NotBeNullOrEmpty("Level field must have a value") |> ignore
-            (x.CreatedAt.Value > DateTime.MinValue).Should().BeTrue("CreatedAt cannot be empty") |> ignore
+            x.CreatedAt.Value.Should().Be(expectedCreatedAt.Value, "CreatedAt must be 1977-01-01") |> ignore
             x.Content.Value.Should().Contain(x.Error.Value.Value, "Content should contain error") |> ignore
             x.Content.Value.Should().Contain(x.Message.Value, "Content should contain message") |> ignore
         ) 
