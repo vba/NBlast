8f7afd74def94edaabb5c8fc15b86a31aca6973f
Victor Bartel <victor.bartel@dgtresor.gouv.fr>
2015-01-13 13:10:14 +0100

    [Client] Make all js dependencies be loaded by require.js

--- NBlast.Client/app/js/main.js
+++ NBlast.Client/app/js/main.js
@@ -6,9 +6,21 @@
             'angular': '../../bower_components/angular/angular',
             'angular-route': '../../bower_components/angular-route/angular-route.min',
             'angular-resource': '../../bower_components/angular-resource/angular-resource.min',
-            'angular-underscore': '../../bower_components/angular-underscore/angular-underscore.min'
+            'angular-underscore': '../../bower_components/angular-underscore/angular-underscore.min',
+            'jquery': '../../bower_components/jquery/dist/jquery',
+            'bootstrap': '../../bower_components/bootstrap/dist/js/bootstrap',
+            'underscore': '../../bower_components/underscore/underscore-min',
         },
         shim: {
+            underscore: {
+                exports: '_'
+            },
+            jquery: {
+                exports: 'jQuery'
+            },
+            bootstrap: {
+                deps: ['jquery']
+            },
             angular: {
                 exports: 'angular'
             },
@@ -19,7 +31,7 @@
                 deps: ["angular"]
             },
             'angular-underscore': {
-                deps: ["angular"]
+                deps: ["angular", 'underscore']
             }
         }
     });
