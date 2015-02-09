f47ebeb922961d1cb7c673360f216d7d9e335996
Victor Bartel <victor.bartel@dgtresor.gouv.fr>
2015-02-09 22:57:21 +0100

    [Api] Add search term action to the search controller

--- NBlast.Api/Controllers/SearcherController.fs
+++ NBlast.Api/Controllers/SearcherController.fs
@@ -15,58 +15,76 @@ type SearcherController(storageReader: IStorageReader,
     inherit ApiController()
 
     static let logger = NLog.LogManager.GetCurrentClassLogger()
-
-    let itemsPerPage = lazy("NBlast.search.hits_per_page" |> configReader.ReadAsInt)
+    let itemsPerPage  = lazy("NBlast.search.hits_per_page" |> configReader.ReadAsInt)
 
     [<HttpGet>]
     [<Route("search")>]
     [<Route("search/{q}")>]
-    member me.Search (q: string) = me.Search(q, 1)
+    member me.Search q = me.Search(q, 1)
 
     [<HttpGet>]
     [<Route("search")>]
     [<Route("search/{p}/{q}")>]
-    member me.Search (q: string, p: int) =
-        let searchQuery = {
-            SearchQuery.GetOnlyExpression q 
-            with Take = Some itemsPerPage.Value 
-                 Skip = Some ((p - 1) * itemsPerPage.Value)
-        }
-        searchQuery |> sprintf "Simple search with %A" |> logger.Debug
-        let result = storageReader.SearchByField(searchQuery)
-        result
+    member me.Search (q, p) =
+        me.Search (q, p, null, new Nullable<_>(), new Nullable<_>(), new Nullable<_>())
 
     [<HttpGet>]
     [<Route("search")>]
     [<Route("search/{p}/{q}/{sf}")>]
-    member me.Search (q: string, 
-                      p: int, 
-                      sf: string,
-                      sr: Nullable<Boolean>,
-                      from: Nullable<DateTime>,
-                      till: Nullable<DateTime>) =
+    member me.Search (q    : string,
+                      p    : int,
+                      sf   : string,
+                      sr   : Nullable<Boolean>,
+                      from : Nullable<DateTime>,
+                      till : Nullable<DateTime>) =
 
         let searchQuery = me.AssembleSearchQuery(q, p, sf, sr, from, till)
-        searchQuery |> sprintf "Complex search with %A" |> logger.Debug
-        let result = storageReader.SearchByField(searchQuery)
+        searchQuery |> sprintf "Search with %A" |> logger.Debug
+        let result = (searchQuery |> storageReader.SearchByField)
         result
+
+    [<HttpGet>]
+    [<Route("term-search")>]
+    member me.TermSearch (t, q) = me.TermSearch(t, q, 1)
+
+    [<HttpGet>]
+    [<Route("term-search")>]
+    member me.TermSearch (t, q, p) =
+        me.TermSearch (t, q, p, null, new Nullable<_>(), new Nullable<_>(), new Nullable<_>())
+
+    [<HttpGet>]
+    [<Route("term-search")>]
+    member me.TermSearch (t    : string,
+                          q    : string,
+                          p    : int,
+                          sf   : string,
+                          sr   : Nullable<Boolean>,
+                          from : Nullable<DateTime>,
+                          till : Nullable<DateTime>) =
+
+        let logField = t |> LogField.ConvertFrom
+        if logField.IsNone then
+            t |> sprintf "Invalid field name %s" |> me.BadRequest :> IHttpActionResult
+        else
+            let searchQuery = me.AssembleSearchQuery(q, p, sf, sr, from, till)
+            searchQuery |> sprintf "Term search with term %s and %A" t |> logger.Debug
+            let result = (searchQuery |> storageReader.SearchByTerm logField.Value) 
+            me.Ok(result) :> IHttpActionResult
 
     [<HttpGet>]
     [<Route("{id}/get")>]
     member me.GetById (id: Guid) = 
-        let result = storageReader.SearchByField({
-                                                    Expression = "id : " + id.ToString()
-                                                    Filter     = None
-                                                    Skip       = None
-                                                    Sort       = None
-                                                    Take       = Some 1
-                                                 })
-        result
+        storageReader.SearchByField(
+            { Expression = "id : " + id.ToString()
+              Filter     = None
+              Skip       = None
+              Sort       = None
+              Take       = Some 1 }
+        )
 
     [<HttpGet>]
     [<Route("count-all")>]
-    member me.CountAll () =
-        storageReader.CountAll()
+    member me.CountAll () = storageReader.CountAll()
 
     member private me.CombineExpression q = 
         SearchQuery.GetOnlyExpression(q)
@@ -77,29 +95,28 @@ type SearcherController(storageReader: IStorageReader,
 
     member private me.CombineSort sf (sr: Nullable<Boolean>) query = 
         let sf = LogField.ConvertFrom(sf)
-        { query with Sort = if sf.IsNone 
-                            then None
-                            else { Field = sf.Value 
+        { query with Sort = if sf.IsNone then None
+                            else { Field   = sf.Value 
                                    Reverse = if sr.HasValue 
-                                                then sr.Value 
-                                                else false} |> Some }
+                                             then sr.Value 
+                                             else false} |> Some }
 
     member private me.CombineFilter (from: Nullable<DateTime>) 
                                     (till: Nullable<DateTime>) 
                                     query = 
-        { query with Filter = if (from.HasValue && till.HasValue) 
-                                then FilterQuery.Between(from.Value, till.Value) |> Some
-                              else if (from.HasValue)
-                                then FilterQuery.After(from.Value)  |> Some
-                              else if (till.HasValue)
-                                then FilterQuery.Before(till.Value) |> Some
-                              else None }
+        { query with 
+            Filter = 
+                if (from.HasValue && till.HasValue) 
+                    then FilterQuery.Between(from.Value, till.Value) |> Some
+                else if (from.HasValue)
+                    then FilterQuery.After(from.Value)  |> Some
+                else if (till.HasValue)
+                    then FilterQuery.Before(till.Value) |> Some
+                else None
+        }
 
     member private me.AssembleSearchQuery (q, p, sf, sr, from, till) = 
         q |> (me.CombineExpression 
                 >> (me.CombinePage p) 
                 >> (me.CombineSort sf sr) 
                 >> (me.CombineFilter from till))
-
-        
-
