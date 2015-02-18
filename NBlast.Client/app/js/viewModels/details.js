d655ed7d04f0811e574d056c1b892d4fc6730d29
Victor Bartel <victor.bartel@dgtresor.gouv.fr>
2015-02-18 22:34:48 +0100

    [Client] Add details view model tests

--- NBlast.Client/app/js/viewModels/details.js
+++ NBlast.Client/app/js/viewModels/details.js
@@ -3,37 +3,33 @@
 
 	var _             = require('underscore'),
 		ko            = require('knockout'),
-		Class         = require('jsface').Class,
 		views         = require('../views'),
 		markupService = require('../services/markup'),
 		searchService = require('../services/search'),
 		DetailsViewModel;
 
 
-	DetailsViewModel = Class(function () {
-		var prototype = {}, $private = {};
-
-		$private.onGetByIdDone = function(found) {
-			this.details(_.first((found || {}).hits));
-		};
-		prototype.constructor = function(uuid) {
+	DetailsViewModel = (function () {
+		function DetailsViewModel (uuid) {
 			if (!_.isString(uuid)) {
 				throw new Error('uuid param must be a string');
 			}
-
 			this.uuid = ko.observable(uuid);
 			this.details = ko.observable({});
+		}
+		var $private = {};
+		$private.onGetByIdDone = function(found) {
+			this.details(_.first((found || {}).hits));
 		};
-		prototype.bind = function() {
+		DetailsViewModel.prototype.bind = function() {
 			searchService
 				.getById(this.uuid())
 				.done($private.onGetByIdDone.bind(this));
 
 			markupService.applyBindings(this, views.getDetails());
 		};
-
-		return prototype;
-	});
+		return DetailsViewModel;
+	})();
 
 	//noinspection JSUnresolvedVariable
 	module.exports = DetailsViewModel;
---
+++ NBlast.Client/test/viewModels/detailsSpec.js
@@ -0,0 +1,52 @@
+(function() {
+	'use strict';
+	var DetailsViewModel = require('../../app/js/viewModels/details'),
+		chai             = require('chai'),
+		fakes            = require('../fakes');
+
+	chai.should();
+	describe('When details view model is in use', function () {
+		it('Should fail view model creation when no params are supplied', function () {
+			// Given
+			// When
+			// Then
+			chai.expect(function () {
+				new DetailsViewModel(123);
+			}).to.throw('uuid param must be a string');
+		});
+		it('Should fail view model creation when no params are supplied', function () {
+			// Given
+			// When
+			// Then
+			chai.expect(function () {
+				new DetailsViewModel();
+			}).to.throw('uuid param must be a string');
+		});
+		it('Should call get details by id and render it via markup service', function () {
+			// Given
+			var mocker            = fakes.mocker(),
+			    uuid              = 'uuid1',
+			    markupService     = require('../../app/js/services/markup'),
+			    searchService     = require('../../app/js/services/search'),
+				sut               = new DetailsViewModel(uuid),
+				getByIdStub       = mocker.stub(searchService, 'getById'),
+			    applyBindingsStub = mocker.stub(markupService, 'applyBindings'),
+				result            = {
+					done: function(callback) {
+						callback({hits: ['some']});
+					}
+				};
+
+			applyBindingsStub.returns(true);
+			getByIdStub.withArgs(uuid).returns(result);
+
+			// When
+			sut.bind();
+
+			// Then
+			sut.details().should.equal('some');
+			getByIdStub.calledOnce.should.equal(true);
+			applyBindingsStub.calledOnce.should.equal(true);
+		});
+	});
+})();
