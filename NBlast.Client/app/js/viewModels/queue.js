6e889428a670838fa95600394de1752ed4d498d7
Victor Bartel <victor.bartel@dgtresor.gouv.fr>
2015-03-10 22:34:51 +0100

    [Client] Fix queue list display

--- NBlast.Client/app/js/viewModels/queue.js
+++ NBlast.Client/app/js/viewModels/queue.js
@@ -1,17 +1,20 @@
 (function () {
 	'use strict';
 
-	var queue             = require('../services/queue'),
-	    ko                = require('knockout'),
+	var ko                = require('knockout'),
+		queue             = require('../services/queue'),
 	    views             = require('../views'),
-	    markupService     = require('../services/markup'),
+	    config            = require('../config'),
 	    Indicator         = require('../tools/indicator'),
+	    markupService     = require('../services/markup'),
+	    SearchViewModel   = require('../viewModels/search'),
 	    QueueViewModel;
 
 	QueueViewModel = (function () {
 		function QueueViewModel () {
-			this.logs = ko.observable([]);
-			this.total = ko.observable(0);
+			this.logs   = ko.observable([]);
+			this.total  = ko.observable(0);
+			this.moment = config.moment();
 		}
 
 		var indicator = new Indicator(), $private = {};
@@ -21,6 +24,12 @@
 			this.total(data.total || 0);
 			indicator.close();
 		};
+
+		QueueViewModel.prototype.defineFoundIcon = function() {
+			var args = [].slice.call(arguments, 0);
+			SearchViewModel.prototype.defineFoundIcon.apply(this, args);
+		};
+
 		QueueViewModel.prototype.bind = function () {
 			indicator.display('Loading ...');
 
@@ -30,6 +39,7 @@
 				.error(indicator.close.bind(indicator))
 				.done($private.onPeekTopDone.bind(this));
 		};
+
 
 		return QueueViewModel;
 	})();
