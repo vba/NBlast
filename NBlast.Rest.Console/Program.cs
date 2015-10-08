d4c7612c6c79aa62d941f2eb036d183e511558f1
Victor Bartel <victor.bartel@dgtresor.gouv.fr>
2015-10-08 13:48:42 +0200

    [Console] Add different outputs as samples

--- NBlast.Rest.Console/Program.cs
+++ NBlast.Rest.Console/Program.cs
@@ -21,6 +21,7 @@ namespace NBlast.Rest.Console
             Log.Logger = new LoggerConfiguration()
                 .WriteTo.File(@"c:\temp\nblast.serilog.log")
                 .WriteTo.Seq("http://localhost.fiddler:9090/")
+//                .WriteTo.Elasticsearch("http://localhost.fiddler:9090/")
                 .MinimumLevel.Verbose()
                 .CreateLogger();
 
@@ -33,7 +34,75 @@ namespace NBlast.Rest.Console
             };
 
             Log.Logger.Debug("Some {id} {@obj}", 1, obj);
-//            Log.Logger.Warning("Some {id}", 1);
+            //            Log.Logger.Warning("Some {id}", 1);
+
+            /*
+            SEQ:                        
+            {
+              "events":[
+                {
+                  "Timestamp":"2015-10-08T13:37:12.9486666+02:00",
+                  "Level":"Debug",
+                  "MessageTemplate":"Some {id} {@obj}",
+                  "Properties":{
+                    "id":1,
+                    "obj":{
+                      "_typeTag":"Entity",
+                      "Id":"level2",
+                      "Parent":{
+                        "_typeTag":"Entity",
+                        "Id":"first",
+                        "Parent":null,
+                        "Children":[
+                        ]
+                      },
+                      "Children":[
+                        {
+                          "_typeTag":"Entity",
+                          "Id":"level3",
+                          "Parent":null,
+                          "Children":[
+                          ]
+                        }
+                      ]
+                    }
+                  }
+                }
+              ]
+            }
+            
+            Elastic:
+
+            {
+              "@timestamp":"2015-10-08T13:32:26.3767970+02:00",
+              "level":"Debug",
+              "messageTemplate":"Some {id} {@obj}",
+              "message":"Some 1 Entity { Id: \"level2\", Parent: Entity { Id: \"first\", Parent: null, Children: [] }, Children: [Entity { Id: \"level3\", Parent: null, Children: [] }] }",
+              "fields":{
+                "id":1,
+                "obj":{
+                  "_typeTag":"Entity",
+                  "Id":"level2",
+                  "Parent":{
+                    "_typeTag":"Entity",
+                    "Id":"first",
+                    "Parent":null,
+                    "Children":[
+                    ]
+                  },
+                  "Children":[
+                    {
+                      "_typeTag":"Entity",
+                      "Id":"level3",
+                      "Parent":null,
+                      "Children":[
+                      ]
+                    }
+                  ]
+                }
+              }
+            }
+            */
         }
     }
 }
