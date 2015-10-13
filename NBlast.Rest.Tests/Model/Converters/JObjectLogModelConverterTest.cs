40536132994ba3dadd3a8a3e44b364afb38cbdc2
Victor Bartel <victor.bartel@dgtresor.gouv.fr>
2015-10-13 16:49:02 +0200

    [Rest] Add object representation of an event in convertion test

--- NBlast.Rest.Tests/Model/Converters/JObjectLogModelConverterTest.cs
+++ NBlast.Rest.Tests/Model/Converters/JObjectLogModelConverterTest.cs
@@ -15,38 +15,51 @@ namespace NBlast.Rest.Tests.Model.Converters
         public void Check_Convert_on_simple_embedded_object()
         {
             // given
-            //var jobject = CreateSimpleEbeddedJsonObject();
+            var jobject = CreateSimpleEbeddedJsonObject();
 
             // 
         }
 
         private JObject CreateSimpleEbeddedJsonObject()
         {
-            var jsonString = @"
-            { events:[{
-                Timestamp:'2015-10-08T13:37:12.9486666+02:00'
-                  Level:Debug,
-                  MessageTemplate: 'Some {id} {@obj},'
-                  Properties:{
-                    id:1,
-                    obj: {
-                        _typeTag:Entity,
-                        Id:level2,
-                        Parent:{
-                            '_typeTag': 'Entity',
-                            Id: 'first',
-                            Parent:null,
-                            Children:[]
-                        },
-                        Children:[{
-                          '_typeTag':'Entity',
-                          'Id':'level3',
-                          Parent:null,
-                          Children:[]
-                        }]
+            var jsonString = JsonConvert.SerializeObject(new {
+                events = new[]
+                {
+                    new
+                    {
+                        Timestamp = "2015-10-08T13:37:12.9486666+02:00",
+                        Level = "Debug",
+                        MessageTemplate = "Some {id} {@obj},",
+                        Properties = new
+                        {
+                            id = 1,
+                            obj = new
+                            {
+                                _typeTag = "Entity",
+                                Id = "Level2",
+                                Parent = new
+                                {
+                                    _typeTag = "Entity",
+                                    Id = "first",
+                                    Parent = new { },
+                                    Children = new object[0]
+                                },
+                                Children = new [] 
+                                {
+                                    new
+                                    {
+                                	    _typeTag = "Entity",
+                                	    Id = "level3",
+                                	    Parent = new object(),
+                                	    Children = new object[0]
+                                	}
+                                }
+                            }
+                        }
                     }
-                  }
-              }]}";
+                }
+            });
+                
             return JsonConvert.DeserializeObject<JObject>(jsonString);
         }
     }
